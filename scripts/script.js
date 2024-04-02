35a9e42a78264787a4809e33ca5854f8ea41b75f
Shahul-TechWizard <shahul.techwizard@gmail.com>
2024-04-03 02:04:19 +0530

    update v1

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,25 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+
+app.use(express.json());
+
+app.get("/proxy", async (req, res) => {
+  try {
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbyXuNxMKrg3ynTribAAcjU_nTiKavACpwLcpCCts2oEsBqp58csJPkl0NEkORQEf7HC/exec"
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbw-ncyxGI02TwbKEV-qi_4Byo9I_GTEuoiV3uhiKzLSlSpPS4mUy2YHELDTUFlBRcoP/exec";
 
